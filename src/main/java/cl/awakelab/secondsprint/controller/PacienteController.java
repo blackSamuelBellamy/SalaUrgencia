ecd641598037183fcbfe1206a2baf587855e90d0
Diego Ortiz <diegortizb@gmail.com>
2023-09-14 20:02:46 -0300

    Controlador Paciente

--- src/main/java/cl/awakelab/secondsprint/controller/PacienteController.java
+++ src/main/java/cl/awakelab/secondsprint/controller/PacienteController.java
@@ -1,2 +1,42 @@
-package cl.awakelab.secondsprint.controller;public class PacienteController {
+package cl.awakelab.secondsprint.controller;
+
+import cl.awakelab.secondsprint.entity.Medico;
+import cl.awakelab.secondsprint.entity.Paciente;
+import cl.awakelab.secondsprint.service.IPacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/paciente")
+public class PacienteController {
+    @Autowired
+    IPacienteService objPacienteService;
+
+    @GetMapping("/listar")
+    public String listarPacientes(Model model){
+        List<Paciente> listaPacientes = objPacienteService.listarPacientes();
+        model.addAttribute("listaPacientes", listaPacientes);
+        return "templateListaPacientes";
+    }
+
+    @GetMapping("/crear")
+    public String formCrearPaciente(){
+        return "templateCrearPaciente";
+    }
+
+    @PostMapping("/crear")
+    public String crearPaciente(@ModelAttribute Paciente paciente){
+        objPacienteService.crearPaciente(paciente);
+        return "redirect:/paciente/listar";
+    }
+
+    @PostMapping("/eliminar/{id}")
+    public String eliminarPaciente(@PathVariable Integer id){
+        objPacienteService.eliminarPaciente(id);
+        return "redirect:/paciente/listar";
+    }
 }
